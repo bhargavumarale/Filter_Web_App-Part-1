91460cfc3e26851bccb9aa0949315b9e41d3cf62
bhargavumarale <79691733+bhargavumarale@users.noreply.github.com>
2021-07-11 17:36:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw()
+{
+image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot()
+{
+    save('myFiltername.jpeg');
+}
